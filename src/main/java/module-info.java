4f168e8c45d11b8dec9f32678e66e29699b61f5e
Momo <immxmmi@gmx.at>
2022-03-15 10:40:03 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module at.technikum.tourplanner {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens at.technikum.tourplanner to javafx.fxml;
+    exports at.technikum.tourplanner;
+}
