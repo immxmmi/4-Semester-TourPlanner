e82e46d80289b526973e82b53066c42156be49a9
Momo <immxmmi@gmx.at>
2022-03-21 13:08:08 +0100

    Main Ordner erstellt

--- src/main/java/at/technikum/tourplanner/MainApplication.java
+++ src/main/java/at/technikum/tourplanner/MainApplication.java
@@ -4,7 +4,7 @@ import at.technikum.tourplanner.database.DBConnectImpl;
 
 import java.sql.Connection;
 
-public class Main {
+public class MainApplication {
     public static void main(String args[]){
         Connection connection = DBConnectImpl.getInstance().getConnection();
 
